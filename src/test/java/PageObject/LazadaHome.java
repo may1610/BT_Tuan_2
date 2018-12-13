866ec17ed0f3a4f4d74d261e74663afb756485f8
Van Nguyen <nguyenkhanhvan.1610@gmail.com>
2018-12-13 20:40:49 +0700

    Commit

--- src/test/java/PageObject/LazadaHome.java
+++ src/test/java/PageObject/LazadaHome.java
@@ -1,8 +1,13 @@
 package PageObject;
 
+import java.util.List;
+
+import javax.xml.xpath.XPath;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 public class LazadaHome {
@@ -18,5 +23,16 @@ public class LazadaHome {
 
 	@FindBy(xpath = "//div[@id=\"topActionHeader\"]//div[@class=\"lzd-nav-search\"]//button")
 	public WebElement btnSearch;
+	
+	@FindBy(xpath="//div[@class=\"lzd-nav-search\"]//form//div[contains(@class, \"suggest-list\")]//a[contains(@class, \"suggest-common\")]//b")
+	public List<WebElement> suggestLst;
 
+    @FindBy(xpath="//div[@data-qa-locator=\"general-products\" and @data-spm=\"list\"]//div[@class=\"c16H9d\"]//a")
+    public List<WebElement> searchtLst;
+    
+    @FindBy(xpath="//div[@id=\"root\"]//li[contains(@class,\"ant-pagination-next\")]")
+    public WebElement nextPage;
+    
+    @FindBy(xpath="//div[@id=\"root\"]//span[contains(text(),\"items found for\")]")
+    public WebElement noResultLbl;
 }
--- src/test/java/Viettel/BT_Tuan_2/Bai_3.java
+++ src/test/java/Viettel/BT_Tuan_2/Bai_3.java
@@ -2,10 +2,23 @@ package Viettel.BT_Tuan_2;
 
 import static org.junit.Assert.assertTrue;
 
+import java.security.Key;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.UIDefaults.LazyValue;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import PageObject.*;
 
@@ -16,17 +29,111 @@ public class Bai_3 {
 	@Before
 	public void initTest() {
 		System.setProperty("webdriver.chrome.driver", "F:\\SOURCE_TEST\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		lazHome = new LazadaHome(driver);
 		driver.get("https://www.lazada.vn/");
 		driver.manage().window().maximize();
 	}
 
+	// Bai 3.1
+	@Ignore
 	@Test
-	public void Search() {
-		lazHome.txtSearch.sendKeys("ppp");
-		lazHome.btnSearch.click();
-		assertTrue(true);
+	public void CheckSuggestList() {
+		lazHome.txtSearch.sendKeys("Pin");
+		WebDriverWait wait = new WebDriverWait(driver, 10); // doi toi da 10s?
+		wait.until(ExpectedConditions.visibilityOfAllElements(lazHome.suggestLst));
+		System.out.println(lazHome.suggestLst);
+		System.out.println(lazHome.suggestLst.size());
+		assertTrue(lazHome.suggestLst.size() == 10);
+		driver.quit();
 	}
 
+	// Bai 3.2
+	@Ignore
+	@Test
+	public void CheckContainKeyword() {
+		String keyWord = "pin";
+		boolean isContainKeyword = true;
+		lazHome.txtSearch.sendKeys(keyWord);
+		WebDriverWait wait = new WebDriverWait(driver, 10); // doi toi da 10s?
+		wait.until(ExpectedConditions.visibilityOfAllElements(lazHome.suggestLst));
+		for (WebElement el : lazHome.suggestLst) {
+			if (!el.getText().toLowerCase().contains(keyWord)) {
+				isContainKeyword = false;
+				break;
+			}
+		}
+
+		assertTrue(isContainKeyword); // sao trong
+		driver.quit();
+	}
+
+	// Bai 3.3
+	@Ignore
+	@Test
+	public void CheckSearchResult() {
+		String keyWord = "máy ép trái cây panasonic";
+		boolean isContainKeyword = true;
+		lazHome.txtSearch.sendKeys(keyWord);
+		lazHome.btnSearch.click();
+		WebDriverWait wait = new WebDriverWait(driver, 20); // doi toi da 10s?
+		wait.until(ExpectedConditions.visibilityOfAllElements(lazHome.searchtLst));
+		for (WebElement el : lazHome.searchtLst) {
+			if (!el.getText().toLowerCase().contains(keyWord)) {
+				isContainKeyword = false;
+				break;
+			}
+		}
+
+		assertTrue(isContainKeyword); // sao trong
+		driver.quit();
+	}
+
+	// Bai 3.3
+	@Test
+	public void CheckSearchResult1() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		Actions actions = new Actions(driver);
+		int i = 1;
+		String notFound = "0 items found for";
+		boolean hasNextPage = true;
+		String keyWord = "aaaa"; // 111111111 --> ko co ket qua nao  ; 5 hộp Pin 2A lớn Maxell nguyên hộp 40 viên. Made in Indonesia --> 1 ket qua
+		boolean isContainKeyword = true;
+		lazHome.txtSearch.sendKeys(keyWord);
+		lazHome.btnSearch.click();
+		if (lazHome.noResultLbl.getText().toLowerCase().contains(notFound)) {
+			System.out.println("Không tìm thấy kết quả nào!");
+			assertTrue(true);
+		} else {
+
+			WebDriverWait wait = new WebDriverWait(driver, 20); // doi toi da 10s?
+			try {
+				wait.until(ExpectedConditions.visibilityOfAllElements(lazHome.searchtLst));
+			} catch (Exception ex) {
+
+			}
+			while (hasNextPage) {
+				System.out.println("Page " + i);
+				for (WebElement el : lazHome.searchtLst) {
+					System.out.println(el.getAttribute("title").toLowerCase());
+					if (!el.getAttribute("title").toLowerCase().contains(keyWord.toLowerCase())) {
+						isContainKeyword = false;
+						break;
+					}
+				}
+
+				js.executeScript("arguments[0].scrollIntoView(false);", lazHome.nextPage);
+				actions.sendKeys(Keys.DOWN);
+				actions.perform();
+				if (lazHome.nextPage.getAttribute("aria-disabled").equals("false")) {
+					lazHome.nextPage.click();
+					i++;
+				} else
+					hasNextPage = false;
+			}
+
+			assertTrue(isContainKeyword); // sao trong
+		}
+		driver.quit();
+	}
 }
